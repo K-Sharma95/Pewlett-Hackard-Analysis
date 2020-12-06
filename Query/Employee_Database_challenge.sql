049aecf5245140b0bd8f6625218b81ee001ac611
unknown <kanav.sharma995@gmail.com>
2020-12-06 16:03:07 -0500

    Uploaded after unique_titles table was created

---
+++ Query/Employee_Database_challenge.sql
@@ -0,0 +1,54 @@
+-- RETRIEVE COLUMNS FROM EMPLOYEES & TITLES TABLE AND SAVE THEM IN NEW TABLES EMP_D1 & RET_D1
+SELECT emp_no, first_name, last_name
+INTO emp_d1
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31');
+
+SELECT emp_no, title, from_date, to_date
+INTO ret_d1
+FROM titles
+
+SELECT * FROM ret_d1
+DROP TABLE emp_d1 CASCADE
+
+-- JOIN BOTH TABLES ON PRIMARY KEY emp_no and order by emp_no, into a new table titles "retirement_titles"
+SELECT em.emp_no,
+	em.first_name, 
+	em.last_name,
+    
+	re.title,
+	re.from_date,
+	re.to_date
+	
+INTO retirement_titles
+		
+FROM emp_d1 as em
+INNER JOIN ret_d1 as re
+ON em.emp_no = re.emp_no
+ORDER BY em.emp_no;
+
+SELECT 
+INTO
+FROM
+
+
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, to_date DESC;
+
+SELECT * FROM unique_titles
+
+
+-- Create a query to retrieve the number of employees bt their most recent job title who are about to retire, and title it retiring_titles.csv
+
+
+
+
+-- 
