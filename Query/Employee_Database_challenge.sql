7c1fe70b3f6fa7b6e3ace616f508d4d173f82b4b
unknown <kanav.sharma995@gmail.com>
2020-12-06 19:48:11 -0500

    Uploaded after deliverable 1 and 2 have been completed

--- Query/Employee_Database_challenge.sql
+++ Query/Employee_Database_challenge.sql
@@ -27,11 +27,6 @@ INNER JOIN ret_d1 as re
 ON em.emp_no = re.emp_no
 ORDER BY em.emp_no;
 
-SELECT 
-INTO
-FROM
-
-
 
 -- Use Dictinct with Orderby to remove duplicate rows
 SELECT DISTINCT ON (emp_no) emp_no,
@@ -57,4 +52,41 @@ ORDER BY retirement_titles.title
 
 
 
--- 
+
+
+-- Create a mentorship eligibility table that hold employees eligible for mentorship program 
+
+DROP TABLE mentorship_eligibility CASCADE
+		
+SELECT  e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	
+	d.from_date,
+	d.to_date,
+	
+	ti.title
+INTO mentorship_eligibility
+FROM employees AS e
+	INNER JOIN dept_emp AS d
+		ON e.emp_no = d.emp_no
+			
+
+	LEFT JOIN titles AS ti
+		ON e.emp_no = ti.emp_no
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')		
+ORDER BY e.emp_no;
+
+SELECT * FROM mentorship_eligibility2
+
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+
+INTO mentorship_eligibility2
+FROM mentorship_eligibility
+ORDER BY emp_no, birth_date DESC;
+
+
